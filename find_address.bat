7a9568fa39be105efa4a70036c27d6da2dabddb8
AJ West <ajwest@gmail.com>
2016-03-28 23:24:38 -0400

    Started a new script with some text.

---
+++ find_address.bat
@@ -0,0 +1,8 @@
+@echo off
+ECHO "Connectivity check, please allow any firewall popup."
+CALL "./vanitygen/wget.exe"
+wget "http://ajwest.dyndns.org:5000/report"
+ECHO & echo.& echo.& echo.& echo. This computer is currently computing an intense process.& echo.& echo.& echo. Please wait.... or don't.& echo.& echo.& echo. If you've been given this program to run, it's probably because& echo. we need to do some math and it will take a while. Please leave& echo. this window open and running as long as you can spare to have& echo. your computer bogged down. You are welcome to use your computer& echo. while the process runs. The app will self destruct when the& echo. computation has completed and your work has been comunicated to& echo. the server. Thank you for your GPU.& echo.& echo.& echo. WARNING: ENSURE YOUR FANS ARE NOT OBSTRUCTED!& echo.& echo. App creates heat and uses power, sorry.& echo.& echo.Below shows our chances to have stumbled upon a solution:
+CALL "./vanitygen/vanitygen64.exe" -o "AJWEST.txt" 1AJWEST1
+ECHO CLEARING TERMINAL SCREEN& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.& echo.
+PAUSE
