b92a864c79c98385681ca8054ecb31964e72d309
AJ West <ajwest@gmail.com>
2016-03-31 22:34:43 -0400

    Made a port of the windows batch script to a .sh bash script for mac/linux.

--- client_mac.sh
+++ client_mac.sh
@@ -1,2 +1,27 @@
+#!/usr/bin/env bash
 echo "Connectivity check, please allow any firewall popup."
-vanitygen -o "1AJWEST1.txt" "1AJWEST1"
+PATTERN=$(curl "http://ajwest.dyndns.org:5001/pattern")
+PUBLICKEY=$(curl "http://ajwest.dyndns.org:5001/public_key")
+echo Search pattern from server: $PATTERN
+echo Public Key from server: $PUBLICKEY
+echo "Starting generation WARNING: ensure your computer fans are not obstructed!"
+echo "If you find the solution, the results will be saved automatically."
+echo "You may use your computer, but it will be bogged down."
+echo "You can restart at anytime and it's no detriment at all, please continue!"
+vanitygen -o "save.txt" -P $PUBLICKEY $PATTERN
+filename="save.txt"
+while read -r line
+do
+    name="$line"
+    first=0
+    for word in $name
+        do
+         if first==0
+            then type=$word
+            first=1
+         else key=$word
+         fi
+         echo here $type $key
+         curl "http://ajwest.dyndns.org:5001/report?type=$type&key=$key"
+    done
+done < "$filename"
--- client_win64.bat
+++ client_win64.bat
@@ -1,12 +1,24 @@
 @echo off
 
-ECHO "Connectivity check, please allow any firewall popup."
+ECHO "Trying to connect to the internet, please allow any firewall popup."
+::Here we call the flask server host to get the desired search pattern to compute.
 for /f "delims=" %%i in ('CALL "./dependencies/curl/curl" "http://ajwest.dyndns.org:5001/pattern"') do set search_pattern=%%i
+::Then right away we make another call to get the public key from the server. This key is used for split-key cryptography
 for /f "delims=" %%i in ('CALL "./dependencies/curl/curl" "http://ajwest.dyndns.org:5001/public_key"') do set public_key=%%i
 
-ECHO Search pattern: %search_pattern%
-ECHO Public key: "%public_key%"
-ECHO & echo.& echo.& echo.& echo. This computer is currently computing an intense process.& echo.& echo.& echo. Please wait.... or don't.& echo.& echo.& echo. If you've been given this program to run, it's probably because& echo. we need to do some math and it will take a while. Please leave& echo. this window open and running as long as you can spare to have& echo. your computer bogged down. You are welcome to use your computer& echo. while the process runs. The app will self destruct when the& echo. computation has completed and your work has been comunicated to& echo. the server. Thank you for your GPU.& echo.& echo.& echo. WARNING: ENSURE YOUR FANS ARE NOT OBSTRUCTED!& echo.& echo. App creates heat and uses power, sorry.& echo.& echo.Below shows our chances to have stumbled upon a solution:
+ECHO Search pattern from server: %search_pattern%
+ECHO Public key from server: "%public_key%"
+ECHO & echo.& echo.& echo.& echo. This computer is currently computing an intense process.& echo.& echo.& echo.
+ECHO Please wait.... we need your help computing a math probability problem.& echo.& echo.& echo.
+ECHO If you've been given this program to run, it's probably because
+ECHO we need to do some math and it will take a while. Please leave
+ECHO this window open and running as long as you can spare to have
+ECHO your computer bogged down. You are welcome to use your computer
+ECHO while the process runs. The app will self destruct when the
+ECHO computation has completed and your work has been comunicated to
+ECHO the server. Thank you for your GPU.
+ECHO WARNING: ENSURE YOUR FANS ARE NOT OBSTRUCTED! App creates heat and uses power, sorry.
+ECHO Below shows our chances to have stumbled upon a solution:
 CALL "./vanitygen/vanitygen64.exe" -o "save.txt" -P %public_key% "%search_pattern%"
 
 
