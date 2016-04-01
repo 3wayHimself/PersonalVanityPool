53251b38259e05497bc2fbc62a146820259e2b06
AJ West <ajwest@gmail.com>
2016-03-31 20:23:58 -0400

    Made more logical endpoints, updating windows batch.

--- client_win64.bat
+++ client_win64.bat
@@ -1,12 +1,13 @@
-@echo off
 ECHO "Connectivity check, please allow any firewall popup."
-for /f "delims=" %%i in ('CALL "./dependencies/curl/curl" "http://ajwest.dyndns.org:5001/report?client=main"') do set search_pattern=%%i
+for /f "delims=" %%i in ('CALL "./dependencies/curl/curl" "http://ajwest.dyndns.org:5001/pattern"') do set search_pattern=%%i
+for /f "delims=" %%i in ('CALL "./dependencies/curl/curl" "http://ajwest.dyndns.org:5001/public_key"') do set public_key=%%i
 ECHO %search_pattern%
+ECHO %search_pattern%
+ECHO Public key: "%public_key%"
 ECHO & echo.& echo.& echo.& echo. This computer is currently computing an intense process.& echo.& echo.& echo. Please wait.... or don't.& echo.& echo.& echo. If you've been given this program to run, it's probably because& echo. we need to do some math and it will take a while. Please leave& echo. this window open and running as long as you can spare to have& echo. your computer bogged down. You are welcome to use your computer& echo. while the process runs. The app will self destruct when the& echo. computation has completed and your work has been comunicated to& echo. the server. Thank you for your GPU.& echo.& echo.& echo. WARNING: ENSURE YOUR FANS ARE NOT OBSTRUCTED!& echo.& echo. App creates heat and uses power, sorry.& echo.& echo.Below shows our chances to have stumbled upon a solution:
-CALL "./vanitygen/vanitygen64.exe" -o "save.txt" "%search_pattern%"
-@echo off
+CALL "./vanitygen/vanitygen64.exe" -o "save.txt" -P %public_key% "%search_pattern%"
 for /f "tokens=*" %%a in (save.txt) do (
-   CALL "./dependencies/curl/curl" "http://ajwest.dyndns.org:5000/report?client=2424%26status=found%26key=%%a"
+   CALL "./dependencies/curl/curl" "http://ajwest.dyndns.org:5001/report?client=2424%26status=found%26key=%%a"
 )
 
 
--- vanity_flask_server/vanity_listen.py
+++ vanity_flask_server/vanity_listen.py
@@ -2,15 +2,16 @@ from flask import Flask, request
 import json
 app = Flask(__name__)
 
-SEARCH_PATTERN = "1AJWEST1" #must start with 1
+SEARCH_PATTERN = "1AJW" #must start with 1
+PUBLIC_KEY = "049106abce7220c8b064db7485977022bfcccf7998ba9a94f984c17b3517e660462fa6c4928b4462393ec4fd6a9d5e6483fc6ec8654810120ccae4e140fb172330" #for creating split private keys for securely computing on untrusted computers/sending over network
 
 @app.route('/')
 def hello_world():
-    return "str"
+    return "your connection is working. call /report then /private_key and /report when you've found it."
 
 
-@app.route('/report')
-def report():
+@app.route('/pattern')
+def pattern():
     """Clients call /report?client=theID&status=connectivitycheck
         We should return the search string so the client can begin
         computing addresses for us.
@@ -18,19 +19,37 @@ def report():
     client_id = request.args.get('client', '')
     status = request.args.get('status', '')
     print('called by client: ' + client_id)
-    print('status is: ' + status)
 
-    if status == 'found':
-        print('PRIVATE KEY FOUND!')
-        pattern = request.args.get('pattern', '')
-        address = request.args.get('address', '')
-        privkey = request.args.get('privkey', '')
-        print(pattern)
-        print(address)
-        print(privkey)
 
     print('returning search pattern: ' + SEARCH_PATTERN)
     return SEARCH_PATTERN
+
+@app.route('/public_key')
+def public_key():
+    """If you are sending the app to friends, you're going to want to generate a public key
+    so your data is encrypted. Otherwise you're basically sending plaintext keys over insecure networks,
+    plus the client computers all potentially have a copy of the keys. TODO: Explain split key pairs."""
+    client_id = request.args.get('client', '')
+    status = request.args.get('status', '')
+    search_pattern = request.args.get('search_pattern', '')
+    if search_pattern:
+        print(search_pattern)
+    print('returning public key: ' + PUBLIC_KEY)
+    return PUBLIC_KEY
+
+@app.route('/report')
+def report():
+    """Here is where the client will call the server when the target vanity address has been found.
+    Combine the private key with the private key you used to generate the PUBLIC_KEY"""
+
+    print('PRIVATE KEY FOUND!')
+    pattern = request.args.get('pattern', '')
+    address = request.args.get('address', '')
+    privkey = request.args.get('privkey', '')
+    print(pattern)
+    print(address)
+    print(privkey)
+    return "YOU FOUND THE ADDRESS! THANK YOU SO MUCH!"
 
 
 if __name__ == '__main__':
