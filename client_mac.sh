6d0a747314d08bffbb2443890ba9f4bae8ddb2ed
AJ West <ajwest@gmail.com>
2016-04-02 01:44:41 -0400

    Fleshed out a lot more text for the user and some comments in the bash script. It's wordy, but people would rather read stuff instead of just waiting on a blank black screen in my opinion.

--- client_mac.sh
+++ client_mac.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 echo "Connectivity check, please allow any firewall popup."
-PATTERN=$(curl "http://ajwest.dyndns.org:5001/pattern")
+# We need to get the search pattern and public key from the server. We could have setup the server to return json
+# and then written some bash code to set the values to the proper variables. However, we have a server and it's just
+# a bit of text, so why not make two separate calls and return plaintext? So we do that, set it to the variable and we
+# can just use that directly later. This also serves to prompt any firewall popup from the client computer so the user
+# can allow the connection, which we'll need later if it ends up solving the key.
+PATTERN=$(curl --silent "http://ajwest.dyndns.org:5001/pattern")
 PUBLICKEY=$(curl "http://ajwest.dyndns.org:5001/public_key")
-echo Search pattern from server: $PATTERN
-echo Public Key from server: $PUBLICKEY
-echo "Starting generation WARNING: ensure your computer fans are not obstructed!"
-echo "If you find the solution, the results will be saved automatically."
-echo "You may use your computer, but it will be bogged down."
-echo "You can restart at anytime and it's no detriment at all, please continue!"
+
+echo " "
+echo " "
+echo "Search pattern from server: $PATTERN"
+echo "Public Key from server: $PUBLICKEY"
+echo " "
+echo "Starting key generation"
+echo " "
+echo "!!! WARNING: Ensure your computer fans are not obstructed! !!!"
+echo " "
+echo "If this is a laptop, you'll need to plug it into a wall outlet."
+echo " "
+echo "If you find the solution, the results will be saved and reported over the internet automatically. You may use your computer while the computation is underway, but it will be bogged down and you won't be able to render complex graphics. You can restart at anytime and it's no detriment at all, please feel free to close for a period and reopen later to continue the search for the solution!"
+echo " "
+echo " "
+echo "Below you can watch the probability of finding the solution in this session. This is NOT a traditional progress bar, it's an idea of the work you've performed in the time since you opened this script."
 vanitygen -o "save.txt" -P $PUBLICKEY $PATTERN
 filename="save.txt"
 while read -r line
